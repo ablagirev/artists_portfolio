dfc1b9a824a2affb6f01e19419d4baccbe6031fd
ablagirev <blagirev.a.v@gmail.com>
2020-02-23 19:10:17 +0300

    loader added to ui kit

--- src/client/ui-kit/index.js
+++ src/client/ui-kit/index.js
@@ -4,5 +4,6 @@ import { Divider } from './Divider'
 import { Spacer } from './Spacer'
 import { Icon } from './Icon'
 import { Breadcrumbs } from './Breadcrumbs'
+import { Loader } from './Loader'
 
-export { Text, Heading, Divider, Spacer, Icon, Breadcrumbs }
+export { Text, Heading, Divider, Spacer, Icon, Breadcrumbs, Loader }
