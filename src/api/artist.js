a1122db23a9148e078bd5df36e65ecd4f3d8771a
konstantin <shepelev.k@gmail.com>
2020-04-12 13:51:55 +0300

    [front] прикрутить апи на фронте added api for artistList and artistDetails

--- src/api/artist.js
+++ src/api/artist.js
@@ -1,24 +1,7 @@
-import { schema, normalize } from 'normalizr'
-import { data } from 'sourceData/artistData'
+import axios from 'axios'
 
-const getArtistList = type => {
-  const artists = data[type] ? data[type] : null
-  console.log(type);
-  return artists
-    ? Promise.resolve({ data: { rows: artists, total: artists.length } })
-    : Promise.reject({ statusCode: 404, message: 'Страница не найдена' })
-}
+const getArtistList = type => axios.get(`/api/${type}`)
 
-const getArtistDetails = (type, artistId) => {
-  const artists = data[type] ? data[type] : []
-  const scheme = new schema.Entity('artist')
-  const {
-    entities: { artist: normalizedData }
-  } = normalize(artists, [scheme])
-
-  return normalizedData && normalizedData[artistId]
-    ? Promise.resolve({ data: normalizedData[artistId] })
-    : Promise.reject({ statusCode: 404, message: 'Страница не найдена' })
-}
+const getArtistDetails = (type, artistId) => axios.get(`/api/${type}/${artistId}`)
 
 export const artistApi = { getArtistList, getArtistDetails }
