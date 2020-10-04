5a0dc358308e381e10069ae9322817ccf6d07637
booandrew <booandrew23@gmail.com>
2020-10-04 13:14:02 +0300

    new meta tags were added

--- src/seo.js
+++ src/seo.js
@@ -3,7 +3,9 @@ import { Helmet } from 'react-helmet'
 
 export const Seo = () => (
   <Helmet>
-    <meta property="og:title" content="Бюро Маши Поповой"></meta>
-    <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/marias-bureau.appspot.com/o/header%2FlogoMobile.png?alt=media&token=57cb878f-a597-46d7-96b8-0d02d38baa58"></meta>
+    <meta property="og:title" content="Бюро Маши Поповой"/>
+    <meta property='og:type' content="article" />
+    <meta property='og:site_name' content="bureaump.ru" />
+    <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/marias-bureau.appspot.com/o/header%2FlogoMobile.png?alt=media&token=57cb878f-a597-46d7-96b8-0d02d38baa58"/>
   </Helmet>
 )
