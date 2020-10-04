016517e1f815c66621f9e60b1f89dd70449fa4e2
booandrew <booandrew23@gmail.com>
2020-10-04 12:57:47 +0300

    react-helmet was installed, seo was added

---
+++ src/seo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+export const Seo = () => (
+  <Helmet>
+    <meta property="og:title" content="Бюро Маши Поповой"></meta>
+    <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/marias-bureau.appspot.com/o/header%2FlogoMobile.png?alt=media&token=57cb878f-a597-46d7-96b8-0d02d38baa58"></meta>
+  </Helmet>
+)
